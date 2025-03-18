eb54cdab64f142e570afbc542c2eaadfc303eff0
AbdulkarimMziya <abdulmziya@gmail.com>
2025-03-18 12:27:30 -0700

    change grid function working correctly

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const container = document.querySelector(".container");
 
+const changeGridBtn = document.querySelector("#changeSizeBtn");
+changeGridBtn.addEventListener("click", changeGridSize);
+
+
 function getUserGridSize() {
     const userInput = prompt("Enter Grid size: ");
     const gridSize = parseInt(userInput);
@@ -20,12 +24,15 @@ function changeGridSize() {
     }
 }
 
-const changeGridBtn = document.querySelector("#changeSizeBtn");
-changeGridBtn.addEventListener("click", changeGridSize);
 
-function createGridItem() {
+function createGridItem(gridSize) {
     const div = document.createElement("div");
     div.classList.add("item");
+
+    const itemSize = `calc(600px / ${gridSize})`;
+    div.style.width = itemSize;
+    div.style.height = itemSize;
+
     return div;
 }
 
@@ -35,16 +42,28 @@ function addHoverEffect(item) {
     });
 }
 
+function clearGrid() {
+    const container = document.querySelector(".container");
+  
+    // Loop through all child elements and remove them
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+}
+
 function createGrid(gridSize = 16) {
+    clearGrid() // Clears any existing grid
 
     const container = document.querySelector(".container");
     let size = Math.pow(gridSize, 2);
     
     for(let i = 1; i <= size; i++){
-      const item = createGridItem(); // Create a new <div> element
+      const item = createGridItem(gridSize); // Create a new <div> element
       addHoverEffect(item);
       container.appendChild(item); // Append the <div> to the container
     }
 }
-  
-createGrid();
+
+window.addEventListener("load", () => {
+    createGrid();
+});
