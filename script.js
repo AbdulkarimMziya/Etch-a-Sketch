71cb453622222253c52ae1b937c3a212840ba2d1
AbdulkarimMziya <abdulmziya@gmail.com>
2025-03-18 10:59:19 -0700

    use helper function for code legibility to create the grid

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const container = document.querySelector(".container");
 
-function createGrid(gridSize) {
-  let array = [];
-  
-  let size = Math.pow(gridSize, 2);
-  for(let i = 1; i <= size; i++){
-    const div = document.createElement("div"); // Create a new <div> element
-    div.classList.add("item"); // Add the class 'item' to the <div>
-    container.appendChild(div); // Append the <div> to the container
-  }
+function createGridItem() {
+    const div = document.createElement("div");
+    div.classList.add("item");
+    return div;
 }
 
-createGrid(16);
+
+function createGrid(gridSize = 16) {
+
+    const container = document.querySelector(".container");
+    let size = Math.pow(gridSize, 2);
+    
+    for(let i = 1; i <= size; i++){
+      const item = createGridItem(); // Create a new <div> element
+      addHoverEffect(item);
+      container.appendChild(item); // Append the <div> to the container
+    }
+}
+  
+createGrid();
